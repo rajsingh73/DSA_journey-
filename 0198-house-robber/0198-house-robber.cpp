ee41d39d792f68358585dc047bab7eaa87bf2d87
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-04-03 21:14:05 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int rob(vector<int>& nums){
-        int prev=nums[0];
+        int prev=0;
         int prev2=0;
-        for(int i=1;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
             int take=nums[i];
             if(i>1){
                 take+=prev2;
