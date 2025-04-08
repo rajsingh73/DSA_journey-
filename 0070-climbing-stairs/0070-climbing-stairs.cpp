38c160db7b5fd957640603c76afc7682f4e2c0dc
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-04-08 12:20:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.23%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> v(n+2);
+        v[1]=1;
+        v[2]=2;
+        for(int i=3;i<=n;i++){
+            v[i]=v[i-1]+v[i-2];
+        }
+        return v[n];
+        
+    }
+};
