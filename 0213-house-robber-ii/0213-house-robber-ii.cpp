e67c0b28828a57f34da09bede2af2fb92ba8f323
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-04-11 02:17:06 +0530

    Time: 0 ms (100%), Space: 10.1 MB (97.34%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
-        int prev=nums[0];
+        int prev1=nums[0];
         int prev2=0;
         for(int i=1;i<nums.size()-1;i++){
             int take=nums[i]+prev2;
-            int nottake=prev;
-            int cur=max(take,nottake);
-            prev2=prev;
-            prev=cur;
+            int notake=prev1;
+            prev2=prev1;
+            prev1=max(take,notake);
         }
-        int firstone=prev;
-        prev=nums[1];
-        prev2=0;
-        for(int i=2;i<nums.size();i++){
-            int take=nums[i]+prev2;
-            int nottake=prev;
-            prev2=prev;
-            int cur=max(take,nottake);
-            prev=cur;
+        int temp=prev1;
+        if(nums.size()>1){
+            prev1=nums[1];
+            prev2=0;
+            for(int i=2;i<nums.size();i++){
+                int take=nums[i]+prev2;
+                int notake=prev1;
+                prev2=prev1;
+                prev1=max(take,notake);
+            }
         }
-        int secondone=prev;
-        return max(firstone,secondone);
+        return max(temp,prev1);
         
     }
 };
