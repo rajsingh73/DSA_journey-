8915504729f85459ccfaa1bb9548b96f3b2dc03a
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-05-18 07:38:44 +0530

    Time: 35 ms (34.75%), Space: 26.8 MB (72.6%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int solve(string &s,string &t,int index1,int index2){
+    int solve(string &s,string &t,int index1,int index2,vector<vector<int>> & dp){
         if(index2<0) return 1;
         if(index1<0) return 0;
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
         int first=0;
         if(s[index1]==t[index2]){
-            first=solve(s,t,index1-1,index2-1);
+            first=solve(s,t,index1-1,index2-1,dp);
         }
-        first+=solve(s,t,index1-1,index2);
-        return first;
+        first+=solve(s,t,index1-1,index2,dp);
+        return dp[index1][index2]= first;
     }
     int numDistinct(string s, string t) {
-        return solve(s,t,s.size()-1,t.size()-1);
+        int n=s.size();
+        int m=t.size();
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        return solve(s,t,s.size()-1,t.size()-1,dp);
     }
 };
