a6e69a8f90da59bf72e5c822be0fe87aa17e9a16
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-05-28 16:21:35 +0530

    Time: 4 ms (22.73%), Space: 15.7 MB (78.3%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,30 +12,33 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,vector<int>>mp;
-        queue<pair<TreeNode*,int>> q;
+        map<int,map<int,vector<int>>>mp;
+        queue<pair<TreeNode*,pair<int,int>>> q;
         vector<vector<int>> ans;
         if(root==NULL) return ans;
-        q.push({root,0});
+        q.push({root,{0,0}});
         int lowest=INT_MAX;
         int maxi=INT_MIN;
         while(!q.empty()){
             int size=q.size();
             for(int i=0;i<size;i++){
-                pair<TreeNode*, int> temp=q.front();
+                pair<TreeNode*, pair<int,int>> temp=q.front();
                 q.pop();
-                TreeNode * te=temp.first;
-                int value=temp.second;
-                if(te->left) q.push({te->left,value-1});
-                if(te->right) q.push({te->right,value+1});
-                mp[value].push_back(te->val);
-                lowest=min(lowest,value);
-                maxi=max(maxi,value);
+                TreeNode *te=temp.first;
+                int vertical=temp.second.first;
+                int horizontal=temp.second.second;
+                if(te->left) q.push({te->left,{vertical-1,horizontal+1}});
+                if(te->right) q.push({te->right,{vertical+1,horizontal+1}});
+                mp[vertical][horizontal].push_back(te->val);
             }
         }
-        for(int i=lowest;i<=maxi;i++){
-            sort(mp[i].begin(),mp[i].end());
-            ans.push_back(mp[i]);
+        for(auto [first,second]:mp){
+            vector<int> temp;
+            for(auto [n,v]:second){
+                sort(v.begin(),v.end());
+                temp.insert(temp.end(),v.begin(),v.end());
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
