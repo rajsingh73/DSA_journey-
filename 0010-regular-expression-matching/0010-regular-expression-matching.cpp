0707bf4594dab057a2404eea023648a45fc36762
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-05-29 21:58:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    bool solve(string &s, string &p, int first, int second, vector<vector<int>> &dp) {
-        if (first < 0 && second < 0) return true;
-        if (second < 0) return false;
-        if (first < 0) {
-            if (p[second] == '*') return solve(s, p, first, second - 1, dp);
+    bool solve(string &s,string &p,int first,int second,vector<vector<int>> &dp){
+        if(first<0 && second<0) return true;
+        if(second<0) return false;
+        if(first<0){
+            if(p[second]=='*') return solve(s,p,first,second-2,dp);
             else return false;
         }
-        if (dp[first][second] != -1) return dp[first][second];
-        if (p[second] == '*') {
-            bool zero_occurrence = solve(s, p, first, second - 1, dp);
-            bool one_or_more = (p[second - 1] == s[first] || p[second - 1] == '.') && solve(s, p, first - 1, second, dp);
-            return dp[first][second] = zero_occurrence || one_or_more;
+        if(dp[first][second]!=-1) return dp[first][second];
+        if(p[second]=='*'){
+            bool notake=solve(s,p,first,second-2,dp);
+            bool take=(s[first]==p[second-1] || p[second-1]=='.') && solve(s,p,first-1,second,dp);
+            return dp[first][second]= take || notake;
         }
-
-        if (p[second] == s[first] || p[second] == '.') {
-            return dp[first][second] = solve(s, p, first - 1, second - 1, dp);
+        if(p[second]=='.' || p[second]==s[first]){
+            return dp[first][second]=solve(s,p,first-1,second-1,dp);
         }
-
-        return dp[first][second] = false;
+        return dp[first][second]= false;
     }
-
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solve(s, p, n - 1, m - 1, dp);
+        int n=s.size();
+        int m=s.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return solve(s,p,n-1,m-1,dp);
+        
     }
 };
