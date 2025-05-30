9eab9efa352ad0f39a2d5dfdb6e4d1683db4348b
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-05-30 15:22:55 +0530

    Time: 0 ms (100%), Space: 19.5 MB (61.93%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,31 +10,36 @@
  */
 class Solution {
 public:
-    void solve(ListNode * list1,ListNode * list2  , ListNode * &ans){
-        if(list1==NULL && list2==NULL) return;
-        if(list1==NULL){
-            ans->next=new ListNode(list2->val);
-            solve(list1,list2->next,ans->next);
-            return;
-        }
-        if(list2==NULL){
-            ans->next=new  ListNode(list1->val);
-            solve(list1->next,list2,ans->next);
-            return;
-        }
-        if(list1->val<=list2->val){
-            ans->next=new  ListNode(list1->val);
-            solve(list1->next,list2,ans->next);
-        }
-        else{
-            ans->next=new  ListNode(list2->val);
-            solve(list1,list2->next,ans->next);
-        }
-    }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode * ans=new ListNode(-1);
-        ListNode * temp=ans;
-        solve(list1,list2,ans);
-        return temp->next;
+        if(list1==NULL) return list2;
+        if(list2==NULL) return list1;
+        ListNode * temp1=list1;
+        ListNode * temp2=list2;
+        ListNode * dummy=new ListNode(-1);
+        ListNode * ans=dummy;
+        while(temp1!=NULL && temp2!=NULL){
+            if(temp1->val<=temp2->val){
+                ans->next=temp1;
+                ans=ans->next;
+                temp1=temp1->next;
+            }
+            else{
+                ans->next=temp2;
+                ans=ans->next;
+                temp2=temp2->next;
+            }
+        }
+        while(temp1!=NULL){
+            ans->next=temp1;
+            ans=ans->next;
+            temp1=temp1->next;
+        }
+        while(temp2!=NULL){
+            ans->next=temp2;
+            ans=ans->next;
+            temp2=temp2->next;
+        }
+        return dummy->next;
+        
     }
 };
