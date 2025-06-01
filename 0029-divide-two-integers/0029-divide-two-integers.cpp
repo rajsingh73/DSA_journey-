b7094b0c9e6f6c1cae6e1d441205f9be02b66bc6
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-06-01 16:36:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int divide(int divident, int divisor){
-        if(divident == divisor){
-            return 1;
-        }
-        if(divisor==1){
-            return divident;
-        }
-        if(divident == INT_MIN && divisor == -1) return INT_MAX;
-        bool sign = true;
-        if(divident < 0 && divisor > 0){
-            sign=false;
-        }
-        if(divident >= 0 && divisor < 0){
-            sign= false;
-        }
-        long n=abs(divident);
-        long m=abs(divisor);
+    int divide(int dividend, int divisor) {
+        if(dividend==divisor) return 1;
+        bool sign=false;
+        if(dividend<0 && divisor>0) sign=true;
+        if(dividend>0 && divisor<0) sign=true;
+        if(dividend==INT_MIN && divisor==-1) return INT_MAX;
+        if(dividend==INT_MIN && divisor==1) return INT_MIN;
+        long n=abs((long)dividend);
+        long d=abs((long)divisor);
         long ans=0;
-        while(n>=m){
-            int count=0;
-            while(n>=(m<<(count+1))){
-                count++;
-            }
+        while(n>=d){
+            long count=0;
+            while(n>=(d<<(count+1))) count++;
             ans=ans+(1<<count);
-            n=n-(m<<count);
+            n=n-(d<<count);
         }
-        ans=sign?ans:(-1*ans);
-        if(ans==INT_MIN) return INT_MIN;
-        if(ans==INT_MAX) return INT_MAX;
-        return ans;
+        if(ans==INT_MAX && sign) return INT_MIN-1;
+        if(ans==INT_MIN && sign) return INT_MAX;
+        return sign?-1*ans:ans;
+        
     }
 };
