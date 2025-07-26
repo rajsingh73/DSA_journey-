4ed8de6974eaace23ba7e10c093743a7e63f6beb
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-07-26 07:52:14 +0530

    Time: 7 ms (5.02%), Space: 16.4 MB (14.57%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> & grid,int i,int j,vector<vector<int>> & dp){
-        if(i==0 && j==0) return grid[i][j];
-        if(i<0 || j<0) return INT_MAX-40000;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int top=grid[i][j]+solve(grid,i-1,j,dp);
-        int left=grid[i][j]+solve(grid,i,j-1,dp);
-        cout<<min(top,left)<<" ";
-        return dp[i][j]= min(top,left);
+    int solve(int row,int col,int n,int m,vector<vector<int>> & dp,vector<vector<int>> & grid){
+        if(row>=n || col>=m) return 1e9;
+        if(row==n-1 && col==m-1) return grid[row][col];
+        if(dp[row][col]!=-1) return dp[row][col];
+        return dp[row][col]=grid[row][col]+min(solve(row+1,col,n,m,dp,grid),solve(row,col+1,n,m,dp,grid));
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 && j==0) dp[i][j]=grid[i][j];
-                else{
-                    int top=INT_MAX-40000;
-                    int left=INT_MAX-40000;
-                    if(i>0) top=grid[i][j]+dp[i-1][j];
-                    if(j>0) left=grid[i][j]+dp[i][j-1];
-                    dp[i][j]=min(top,left);
-                }
-            }
-        }
-        return dp[n-1][m-1];
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        return solve(0,0,n,m,dp,grid);
+
     }
 };
