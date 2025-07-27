4dd2eaf3bf43f9351490b15a227deaa8abd515c5
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-07-27 09:29:50 +0530

    Time: 0 ms (100%), Space: 8 MB (85.39%) - LeetHub

--- 0065-valid-number/0065-valid-number.cpp
+++ 0065-valid-number/0065-valid-number.cpp
@@ -1,7 +1,36 @@
+#include <cctype>
+using namespace std;
 class Solution {
 public:
     bool isNumber(string s) {
-        regex pattern(R"(^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$)");
-        return regex_match(s, pattern);
+        int n = s.size();
+        int i = 0;
+        while (i < n && s[i] == ' ') i++;
+        if (i < n && (s[i] == '+' || s[i] == '-')) i++;
+        bool isNumeric = false;
+        bool hasDot = false;
+        bool hasExp = false;
+        while (i < n) {
+            char c = s[i];
+            if (isdigit(c)) {
+                isNumeric = true;
+            }
+            else if (c == '.') {
+                if (hasDot || hasExp) return false;
+                hasDot = true;
+            }
+            else if (c == 'e' || c == 'E') {
+                if (hasExp || !isNumeric) return false;
+                hasExp = true;
+                isNumeric = false;
+                if (i + 1 < n && (s[i + 1] == '+' || s[i + 1] == '-')) i++;
+            }
+            else {
+                return false;
+            }
+
+            i++;
+        }
+        return isNumeric;
     }
 };
