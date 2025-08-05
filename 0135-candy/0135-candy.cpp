41e1006c7eb1b382b4f36e1e244bb0a35b2a2cb3
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-08-05 21:11:15 +0530

    Time: 0 ms (100%), Space: 23.6 MB (9.95%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int sum=1;
+        int n=ratings.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        left[0]=1;
         int i=1;
-        while(i<ratings.size()){
-            if(ratings[i]==ratings[i-1]){
-                sum=sum+1;
-                i++;
-                continue;
-            }
-            int peak=1;
-            while(i<ratings.size() && ratings[i]>ratings[i-1]){
-                peak++;
-                sum=sum+peak;
-                i++;
-            }
-            int down=1;
-            while(i<ratings.size() && ratings[i]<ratings[i-1]){
-                sum=sum+down;
-                down++;
-                i++;
-            }
-            if(down>peak){
-                sum=sum+(down-peak);
-            }
-        } 
+        int sum=0;
+        while(i<n){
+            if(ratings[i]<=ratings[i-1]) left[i]=1;
+            else left[i]=left[i-1]+1;
+            i++;
+        }
+        right[n-1]=1;
+        i=n-2;
+        while(i>=0){
+            if(ratings[i]<=ratings[i+1]) right[i]=1;
+            else right[i]=right[i+1]+1;
+            i--;
+        }
+        for(int i=0;i<n;i++){
+            sum+=max(right[i],left[i]);
+        }
         return sum;
+
+
     }
 };
