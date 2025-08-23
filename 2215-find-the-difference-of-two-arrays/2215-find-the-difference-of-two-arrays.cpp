9aacec86a88f61480d834280b23ec9c07b170dd0
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-08-23 12:07:23 +0530

    Time: 31 ms (27.25%), Space: 36.7 MB (82.3%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        int i=0;
-        int j=0;
+        map<int,int> mp;
         vector<int> first;
         vector<int> second;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]==nums2[j]){
-                int temp=nums1[i];
-                while(i<nums1.size() && nums1[i]==temp) i++;
-                temp=nums2[j];
-                while(j<nums2.size() && nums2[j]==temp) j++;
+        for(auto it: nums1) mp[it]=1;
+        for(auto it: nums2){
+            if(mp.find(it)==mp.end()) {
+                second.push_back(it);
+                mp[it]=2;
             }
-            else if(nums1[i]<nums2[j]){
-                int temp=nums1[i];
-                while(i<nums1.size() && nums1[i]==temp) i++;
-                first.push_back(temp);
-            }
-            else{
-                int temp=nums2[j];
-                while(j<nums2.size() && nums2[j]==temp) j++;
-                second.push_back(temp);
-            }
+            else mp[it]=2;
         }
-        while(i<nums1.size()){
-            int temp=nums1[i];
-            while(i<nums1.size() && nums1[i]==temp) i++;
-            first.push_back(temp);
-        }
-        while(j<nums2.size()){
-            int temp=nums2[j];
-            while(j<nums2.size() && nums2[j]==temp) j++;
-            second.push_back(temp);
+        for(auto it: mp){
+            if(it.second==1) first.push_back(it.first);
         }
         return {first,second};
-        
     }
 };
