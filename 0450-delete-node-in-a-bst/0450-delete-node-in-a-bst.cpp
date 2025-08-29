f3905ee160dd3816bb7f2239a1648872c9e608fd
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-08-29 19:28:07 +0530

    Time: 0 ms (100%), Space: 34.2 MB (63.51%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,27 +11,36 @@
  */
 class Solution {
 public:
-    TreeNode* solve(TreeNode* root){
-        if(root->left==NULL) return root->right;
-        if(root->right==NULL) return root->left;
-        TreeNode * findleftrigh=findleftright(root->left);
-        findleftrigh->right=root->right;
-        return root->left;
-    }
-    TreeNode * findleftright(TreeNode * root){
-        TreeNode * head=root;
-        while(head->right!=NULL){
-            head=head->right;
-        }
-        return head;
+    TreeNode * find(TreeNode * root,int key){
+        if(root==NULL) return NULL;
+        if(root->right && root->right->val==key) return root;
+        if(root->left && root->left->val==key) return root;
+        TreeNode * left=find(root->left,key);
+        TreeNode * right=find(root->right,key);
+        if(left==NULL) return right;;
+        return left;;
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root==NULL) return NULL;
         if(root->val==key){
-            return solve(root);
+            return deleteN(root);
         }
-        root->left=deleteNode(root->left,key);
-        root->right=deleteNode(root->right,key);
+        TreeNode * f=find(root,key);
+        if(f==NULL) return root;
+        if(f->left && f->left->val==key) f->left=deleteN(f->left);
+        else f->right=deleteN(f->right);
         return root;
+
+    }
+    TreeNode * findright(TreeNode * root){
+        if(root->right==NULL) return root;
+        return findright(root->right);
+
+    }
+    TreeNode* deleteN(TreeNode * root){
+        if(root->left==NULL) return root->right;
+        TreeNode * rightmost=findright(root->left);
+        rightmost->right=root->right;
+        return root->left;
     }
 };
