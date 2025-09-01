d3005a9b8ef80710eec469de0b78fcf1c857b72d
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-09-01 21:10:52 +0530

    Time: 0 ms (100%), Space: 10.3 MB (6.13%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,82 +1,73 @@
 class Solution {
 public:
-    void solve(string digits,int index,vector<string> & ans,string s){
+    void solve(vector<string> & ans,int index,string s,string digits){
         if(index==digits.size()){
             ans.push_back(s);
             return;
         }
         if(digits[index]=='2'){
-            for(int i=97;i<=99;i++){
-                char ch=(char)i;
-                s.push_back(ch);
-                solve(digits,index+1,ans,s);
+            for(char c='a'; c<='c';c++){
+                s.push_back(c);
+                solve(ans,index+1,s,digits);
                 s.pop_back();
             }
         }
-        if(digits[index]=='3'){
-            for(int i=100;i<=102;i++){
-                char ch=(char)i;
-                s.push_back(ch);
-                solve(digits,index+1,ans,s);
+        else if(digits[index]=='3'){
+            for(char c='d'; c<='f';c++){
+                s.push_back(c);
+                solve(ans,index+1,s,digits);
                 s.pop_back();
             }
         }
-        if(digits[index]=='4'){
-            for(int i=103;i<=105;i++){
-                char ch=(char)i;
-                s.push_back(ch);
-                solve(digits,index+1,ans,s);
+        else if(digits[index]=='4'){
+            for(char c='g';c<='i';c++){
+                s.push_back(c);
+                solve(ans,index+1,s,digits);
                 s.pop_back();
             }
         }
-        if(digits[index]=='5'){
-            for(int i=106;i<=108;i++){
-                char ch=(char)i;
-                s.push_back(ch);
-                solve(digits,index+1,ans,s);
+        else if(digits[index]=='5'){
+            for(char c='j'; c<='l';c++){
+                s.push_back(c);
+                solve(ans,index+1,s,digits);
                 s.pop_back();
             }
         }
-        if(digits[index]=='6'){
-            for(int i=109;i<=111;i++){
-                char ch=(char)i;
-                s.push_back(ch);
-                solve(digits,index+1,ans,s);
+        else if(digits[index]=='6'){
+            for(char c='m'; c<='o';c++){
+                s.push_back(c);
+                solve(ans,index+1,s,digits);
                 s.pop_back();
             }
         }
-        if(digits[index]=='7'){
-            for(int i=112;i<=115;i++){
-                char ch=(char)i;
-                s.push_back(ch);
-                solve(digits,index+1,ans,s);
+        else if(digits[index]=='7'){
+            for(char c='p'; c<='s';c++){
+                s.push_back(c);
+                solve(ans,index+1,s,digits);
                 s.pop_back();
             }
         }
-        if(digits[index]=='8'){
-            for(int i=116;i<=118;i++){
-                char ch=(char)i;
-                s.push_back(ch);
-                solve(digits,index+1,ans,s);
+        else if(digits[index]=='8'){
+            for(char c='t'; c<='v';c++){
+                s.push_back(c);
+                solve(ans,index+1,s,digits);
                 s.pop_back();
             }
         }
-        if(digits[index]=='9'){
-            for(int i=119;i<=122;i++){
-                char ch=(char)i;
-                s.push_back(ch);
-                solve(digits,index+1,ans,s);
+        else if(digits[index]=='9'){
+            for(char c='w'; c<='z';c++){
+                s.push_back(c);
+                solve(ans,index+1,s,digits);
                 s.pop_back();
             }
         }
-
     }
     vector<string> letterCombinations(string digits) {
-        if(digits.size()==0) return {};
         vector<string> ans;
-        string s="";
+        if(digits.size()==0) return ans;
+        string temp="";
         int index=0;
-        solve(digits,index,ans,s);
+        solve(ans,index,temp,digits);
         return ans;
         
     }
