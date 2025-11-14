a50ad1a9d928ddeb2d0222c0c8d2f98fb6d160da
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2025-11-14 14:53:00 +0530

    Time: 43 ms (48.59%), Space: 98.5 MB (7.21%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<pair<char,int>> row(n,{'s',0});
-        vector<pair<char,int>> col(n,{'s',0});
+        vector<vector<int>> diff(n+1,vector<int>(n+1,0));
         vector<vector<int>> ans(n,vector<int>(n,0));
-        vector<vector<int>> diff(n,vector<int>(n,0));
         for(auto it: queries){
             int row1=it[0],col1=it[1],row2=it[2],col2=it[3];
-            for(int i=row1;i<=row2;i++){
-                diff[i][col1]+=1;
-                if(col2+1<n) diff[i][col2+1]-=1;
-            }
+            diff[row1][col1]++;
+            diff[row1][col2+1]--;
+            diff[row2+1][col1]--;
+            diff[row2+1][col2+1]++;
         }
         for(int i=0;i<n;i++){
-            int pre=0;
             for(int j=0;j<n;j++){
-                pre+=diff[i][j];
-                ans[i][j]=pre;
+                if(i>0) diff[i][j]+=diff[i-1][j];
+                if(j>0) diff[i][j]+=diff[i][j-1];
+                if(i>0 && j>0) diff[i][j]-=diff[i-1][j-1];
+                ans[i][j]=diff[i][j];
             }
         }
         return ans;
