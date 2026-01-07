536a2ad44fd5a504d8e9f9f07ebef043819db8f9
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2026-01-07 16:07:40 +0530

    Time: 6 ms (68.92%), Space: 14.2 MB (22.09%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int solve(string & word1,string & word2,int index1,int index2,vector<vector<int>> & dp){
-        if(index1<0) return index2+1;
-        if(index2<0) return index1+1;
-        if(dp[index1][index2]!=-1) return dp[index1][index2];
-        if(word1[index1]==word2[index2]){
-            return dp[index1][index2]= solve(word1,word2,index1-1,index2-1,dp);
-        }
-        int first=1+solve(word1,word2,index1,index2-1,dp); //insert
-        int second=1+solve(word1,word2,index1-1,index2,dp); //delete
-        int third=1+solve(word1,word2,index1-1,index2-1,dp); //replace
-        return dp[index1][index2]= min(first,min(second,third));
+    int solve(string & word1,string & word2,int row,int col,vector<vector<int>> & dp){
+        if(row<0) return col+1;
+        if(col<0) return row+1;
+        if(dp[row][col]!=-1) return dp[row][col];
+        if(word1[row]==word2[col]) return dp[row][col]=solve(word1,word2,row-1,col-1,dp);
+        int first=1+solve(word1,word2,row,col-1,dp);
+        int second=1+solve(word1,word2,row-1,col-1,dp);
+        int third=1+solve(word1,word2,row-1,col,dp);
+        return dp[row][col]=min({first,second,third});
     }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return solve(word1,word2,word1.size()-1,word2.size()-1,dp);
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return solve(word1,word2,n-1,m-1,dp);
     }
 };
