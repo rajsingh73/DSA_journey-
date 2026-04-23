8a7a85540d732015fa58ca73fc899a25824b3b15
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2026-04-23 11:06:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -4,23 +4,12 @@ class Solution {
         int half=(nums1.length+nums2.length)/2;
         int l=0,r=0,first=0,second=0;
         while(i<=half){
-            if((r>=nums2.length)){
-                second=first;
-                first=nums1[l];
-                l++;
-            }
-            else if(l>=nums1.length){
-                second=first;
-                first=nums2[r];
-                r++;
-            }
-            else if(nums1[l]<=nums2[r]){
-                second=first;
+            second=first;
+            if( (l < nums1.length && (r >= nums2.length || nums1[l] <= nums2[r]))){
                 first=nums1[l];
                 l++;
             }
             else{
-                second=first;
                 first=nums2[r];
                 r++;
             }
@@ -28,5 +17,6 @@ class Solution {
         }
         if((nums1.length+nums2.length)%2==0) return (first+second)/2.0;
         else return first;
+        
     }
 }
