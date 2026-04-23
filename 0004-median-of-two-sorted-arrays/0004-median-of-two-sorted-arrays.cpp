6cd00bf7736cc9fb5f10acb4f496f85ea566bd2e
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2026-04-23 10:51:00 +0530

    Time: 0 ms (100%), Space: 95.2 MB (42.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,67 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double first=-1;
-        double second=-1;
-        int count=0;
         int i=0;
-        int j=0;
-        int total=nums1.size()+nums2.size();
-        if(total==1){
-            return nums1.size()?nums1[0]:nums2[0];
-        }
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]){
-                if(first==-1) first=nums1[i];
-                else if(second==-1) second=nums1[i];
-                else{
-                    first=second;
-                    second=nums1[i];
-                }
-                i++;
+        int half=(nums1.size()+nums2.size())/2;
+        int first=0;
+        int second=0;
+        int l=0;
+        int r=0;
+        while(i<=half){
+            if(r>=nums2.size()){
+                second=first;
+                first=nums1[l];
+                l++;
+            }
+            else if(l>=nums1.size()){
+                second=first;
+                first=nums2[r];
+                r++;
+            }
+            else if(nums1[l]<=nums2[r]){
+                second=first;
+                first=nums1[l];
+                l++;
             }
             else{
-                if(first==-1) first=nums2[j];
-                else if(second==-1) second=nums2[j];
-                else{
-                    first=second;
-                    second=nums2[j];
-                }
-                j++;
-            }
-            count++;
-            if(count==(total/2)+1){
-                return total%2==0?(first+second)/2.0:second;
-            }
-
-        }
-        while(i<nums1.size()){
-            if(first==-1) first=nums1[i];
-            else if(second==-1) second=nums1[i];
-            else{
-                first=second;
-                second=nums1[i];
-            }
-            count++;
-            if(count==(total/2)+1){
-                return total%2==0?(first+second)/2.0:second;
+                second=first;
+                first=nums2[r];
+                r++;
             }
             i++;
         }
-        while(j<nums2.size()){
-            cout<<"HELLO";
-            if(first==-1) first=nums2[j];
-            else if(second==-1) second=nums2[j];
-            else{
-                first=second;
-                second=nums2[j];
-            }
-            count++;
-            if(count==(total/2)+1){
-                return total%2==0?(first+second)/2.0:second;
-            }
-            j++;
-        }
-        return first;
+        if((nums1.size()+nums2.size())%2==0) return (first+second)/2.0;
+        else return first;
     }
 };
