a79be1195911b3a43727eaf01d0a8243cee34b85
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2026-05-29 16:56:45 +0530

    Time: 175 ms (20%), Space: 153.1 MB (20.66%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -1,47 +1,18 @@
 class Solution {
 public:
     int maximumRobots(vector<int>& chargetimes, vector<int>& runningcosts, long long budget) {
+        long long l=0,sum=0,n=chargetimes.size();
+        multiset<int> s;
         int i=0;
-        int ans=0;
-        int j=0;
-        int n=chargetimes.size();
-        int maxicharg=chargetimes[0];
-        long long sumrun=runningcosts[0];
-        while(j<n){
-            long long eq=maxicharg+(j-i+1)*sumrun;
-            if(eq>budget){
-                if(i==j){
-                    j++;
-                    i++;
-                    if(j == n) break;
-                    maxicharg=chargetimes[j];
-                    sumrun=runningcosts[j];
-                }
-                else{
-                    int maxi=0;
-                    for(int k=i+1;k<=j;k++){
-                        maxi=max(chargetimes[k],maxi);
-                    }
-                    maxicharg=maxi;
-                    sumrun-=runningcosts[i];
-                    i++;
-                }
-            }
-            else{
-                ans=max(ans,j-i+1);
-                j++;
-                if(j<n){
-                    maxicharg=max(maxicharg,chargetimes[j]);
-                    sumrun+=runningcosts[j];
-                    // j++;
-                }
+        for(int j=0;j<n;j++){
+            sum+=runningcosts[j];
+            s.insert(chargetimes[j]);
+            if(*rbegin(s)+sum*(j-i+1)>budget){
+                sum-=runningcosts[i];
+                s.erase(s.find(chargetimes[i]));
+                i++;
             }
         }
-        // cout<<i<<" "<<j;
-        // long long eq=maxicharg+(j-i+1)*sumrun;
-        // if(eq <= budget) ans = max(ans, j-i+1);
-        // return max(ans, j-i + 1);
-        return ans;
-
+        return n-i;
     }
 };
