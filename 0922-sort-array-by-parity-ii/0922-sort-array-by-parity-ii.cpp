18c713a0f8b9eb2e81145ba5ff5e4162957551c1
RajtheDev <149926819+rajsingh73@users.noreply.github.com>
2026-09-07 10:11:22 +0530

    Time: 0 ms (100%), Space: 25.9 MB (76.29%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int i=0;
+        int j=1;
+        while(i<nums.size() && j<nums.size()){
+            while(i<nums.size() && nums[i]%2==0) i+=2;
+            while(j<nums.size() && nums[j]%2==1) j+=2;
+            if(i<nums.size() && j<nums.size()){
+                swap(nums[i],nums[j]);
+                i+=2;
+                j+=2;
+            }
+        }
+        return nums;
+        
+    }
+};
